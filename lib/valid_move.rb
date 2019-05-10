00bf6a4d537560158b5586c294dada57ba028adc
David Cantoran <davidcantoran36@gmail.com>
2019-05-10 01:39:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, idx)
   position = idx.to_i - 1 
   
-  if !position_taken?(board, position) && position.between?(0,8)
+  if position_taken?(board, position) == false && position.between?(0,8)
     true 
   elsif !position.between?(0,8)
     false 
